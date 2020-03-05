826bbf36a264b690fa9c00820af8766b6c4cfb67
strbtn <strbtn@hotmail.com>
2020-03-05 14:01:48 +0800

    添加项目文件。

---
+++ Models/IStudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+  public interface IStudentRepository
+    {
+        Student GetStudent(int Id);
+      //  Student GetById(int id);
+
+        //Student Add(Student student);
+
+        //IEnumerable<Student> GetAll();
+
+        //Student Update(Student updatedStudent);
+
+        //Student Delete(int id);
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    /// <summary>
+    /// 学生模型
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        ///姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// 班级
+        /// </summary>
+        public string ClassName { get; set; }
+    }
+}
